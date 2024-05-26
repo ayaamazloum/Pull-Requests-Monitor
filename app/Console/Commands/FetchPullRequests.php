0de3555a4176164327a5615649c2a9415abb14ef
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-26 11:20:01 +0300

    Implementing fetch and save function for old pull requests

--- app/Console/Commands/FetchPullRequests.php
+++ app/Console/Commands/FetchPullRequests.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Services\GitHubService;
 
 class FetchPullRequests extends Command
 {
@@ -16,12 +17,34 @@ class FetchPullRequests extends Command
         parent::__construct();
         $this->githubService = $githubService;
     }
-
-    /**
-     * Execute the console command.
-     */
+    
     public function handle()
     {
-        //
+        $this->fetchAndSave('Old Pull Requests', '1-old-pull-requests.txt', [$this->githubService, 'getOldPullRequests']);
+    }
+
+    protected function fetchAndSave($title, $filename, $fetchMethod)
+    {
+        $this->info("Fetching $title...");
+        $pullRequests = call_user_func($fetchMethod);
+
+        if ($pullRequests && isset($pullRequests['items'])) {
+            $data = array_map(function ($pr) {
+                return [
+                    'PR#' => $pr['number'],
+                    'Title' => $pr['title'],
+                    'URL' => $pr['html_url'],
+                ];
+            }, $pullRequests['items']);
+
+            $content = array_reduce($data, function ($carry, $item) {
+                return $carry . "PR# {$item['PR#']}, Title: {$item['Title']}, URL: {$item['URL']}\n";
+            }, '');
+
+            file_put_contents(storage_path("app/$filename"), $content);
+            $this->info("Saved $title to $filename");
+        } else {
+            $this->warn("No data found for $title");
+        }
     }
 }
